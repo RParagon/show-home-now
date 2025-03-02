8ff8d98c57de96ea30bdd6f6e0d73b72adece623
Rafael Paragon <rvirtualmark2@gmail.com>
2025-03-02 04:51:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    strictPort: true
+  },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   }
