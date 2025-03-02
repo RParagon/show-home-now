9959b277add2bd9f35df3b7db0f8bbaddaa3e6ad
Rafael Paragon <rvirtualmark2@gmail.com>
2025-03-02 05:06:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
+    emptyOutDir: true,
     rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
       output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
       }
     }
-  },
-  server: {
-    port: 3000,
-    strictPort: true
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
   }
 })
